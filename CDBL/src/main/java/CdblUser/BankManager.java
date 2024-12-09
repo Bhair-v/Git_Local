f2088f751262458aa29682bc97abd8771fb04bda
Bhair-v <2220678@iub.edu.bd>
2024-12-09 20:04:25 +0600

    h

--- CDBL/src/main/java/CdblUser/BankManager.java
+++ CDBL/src/main/java/CdblUser/BankManager.java
@@ -1,4 +1,56 @@
 package CdblUser;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class BankManager {
+    @javafx.fxml.FXML
+    private Button AssignRelationshipOfficerButton;
+    @javafx.fxml.FXML
+    private TextField BankTextField;
+    @javafx.fxml.FXML
+    private Button openBOaccountButton;
+    @javafx.fxml.FXML
+    private TextField BankManagerIDTextField;
+    @javafx.fxml.FXML
+    private Button generateTaxReportButton;
+    @javafx.fxml.FXML
+    private Button GenarateComplianceReportButton;
+    @javafx.fxml.FXML
+    private TextField BankBranchTextField;
+    @javafx.fxml.FXML
+    private Button updateClientInfoButton;
+    @javafx.fxml.FXML
+    private Button RespondToInquiryButton;
+    @javafx.fxml.FXML
+    private Button TransferSecuritiesButton;
+
+    @javafx.fxml.FXML
+    public void onActionTransferSecuritiesButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionOpenBOaccountButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActiongenerateTaxReportButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionAssignRelationshipOfficerButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionRespondToInquiryButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionGenarateComplianceReportButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionupdateClientInfoButtonClick(ActionEvent actionEvent) {
+    }
 }
--- CDBL/src/main/java/CdblUser/IndividualInvestor.java
+++ CDBL/src/main/java/CdblUser/IndividualInvestor.java
@@ -1,4 +1,59 @@
 package CdblUser;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class IndividualInvestor {
+    @javafx.fxml.FXML
+    private Button ViewPortofolioButton;
+    @javafx.fxml.FXML
+    private Button SubmitInquiryButton;
+    @javafx.fxml.FXML
+    private TextField BankTextField;
+    @javafx.fxml.FXML
+    private Button ViewCorporateActionsButton;
+    @javafx.fxml.FXML
+    private Button ViewMarketTrendsButton;
+    @javafx.fxml.FXML
+    private Button ViewTransactionHistoryButton;
+    @javafx.fxml.FXML
+    private Button PlaceSellOrderButton;
+    @javafx.fxml.FXML
+    private TextField InvestorIDTextField;
+    @javafx.fxml.FXML
+    private Button RequestInfoUpdateButton;
+    @javafx.fxml.FXML
+    private Button PlaceBuyOrderButton;
+
+    @javafx.fxml.FXML
+    public void onActionRequestInfoUpdateButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionViewPortofolioButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionViewMarketTrendsButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionSubmitInquiryButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionPlaceSellOrderButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionViewTransactionHistoryButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionPlaceBuyOrderButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionViewCorporateActionsButtonClick(ActionEvent actionEvent) {
+    }
 }
